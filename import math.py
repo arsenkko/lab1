19435303091a587d9df552587e6c4bd095d8c9a6
arsenkko <76399068+arsenkko@users.noreply.github.com>
2024-09-15 20:48:55 +0300

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,8 @@
+import math
+
+x = 12.394
+y = 7.139
+n = math.sqrt(math.atan(x))
+m = (x + y) ** math.log(x)
+result = m+n
+print(result)
---
+++ lab1.py
@@ -0,0 +1,19 @@
+book = "rich father poor father"
+print (book)
+is_adult = True
+is_child = False 
+#Виведіть результати логічних операцій.  
+
+print(f"and operanion = {is_child and is_adult}")
+print(f"or operation = {is_child or is_adult}")
+print(f"not is_child operation = {not is_child}")
+print(f"not is_adult operation = {not is_adult}")
+print(f"xor operation = {is_child ^ is_adult}")
+
+
+
+is_sunny = True
+is_warm = False
+#Виведіть результати логічного "і". 
+
+print(f"and operation = {is_sunny and is_warm}")
